0dc2eba98baa65237c6550ee5bf7edb01d7972a8
akbarhlubis <akbar_hamonangan_lubis@teknokrat.ac.id>
2023-10-07 08:32:55 +0700

    add card and variable for home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Book;
+use App\Models\User;
+use App\Models\BorrowBook;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -9,8 +11,13 @@ class HomeController extends Controller
     /**
      * Handle the incoming request.
      */
+    private $title = 'Home';
     public function __invoke(Request $request)
     {
-        return view('pages.home');
+        $data['title'] = str($this->title)->plural();
+        $totalBooks = Book::count();
+        $totalUsers = User::count();
+        $totalBooksBorrowed = BorrowBook::count();
+        return view('pages.home', compact('totalBooks', 'totalUsers', 'totalBooksBorrowed'))->with($data);
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,9 +1,33 @@
 @extends('layouts.app')
 
-@section('title')
-    Home
-@endsection
+@section('title', $title)
 
 @section('body')
-    <h1>Home</h1>
+    <div class="row min-vh-100">
+        <div class="col-lg-12">
+            <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                <h1>{{ $title }}</h1>
+            </div>
+            <div class="d-flex">
+                <div class="card mr-2" style="width: 18rem;">
+                    <div class="card-body">
+                        <h3 class="card-title">Total Buku</h3>
+                        <h1 class="font-weight-bold text-primary">{{ $totalBooks }}</h1>
+                    </div>
+                </div>
+                <div class="card mr-2" style="width: 18rem;">
+                    <div class="card-body">
+                        <h3 class="card-title">Total User</h3>
+                        <h1 class="font-weight-bold text-primary">{{ $totalUsers }}</h1>
+                    </div>
+                </div>
+                <div class="card mr-2" style="width: 18rem;">
+                    <div class="card-body">
+                        <h3 class="card-title">Total Buku Dipinjam </h3>
+                        <h1 class="font-weight-bold text-primary">{{ $totalBooksBorrowed }}</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
